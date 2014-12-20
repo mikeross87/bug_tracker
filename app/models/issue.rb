02446dc88eade7c3195f469fc9d63d30ca99d22e
Michael Ross <michael.b.ross87@gmail.com>
2014-12-19 21:19:09 -0800

    tests

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,2 +1,7 @@
 class Issue < ActiveRecord::Base
+	validates :client, :name, :date, :content, presence: true
+    validates :image, allow_blank: true, format: {
+    	with: %r{\.(gif|jpg|png)\Z}i,
+		message: 'must be a URL for GIF, JPG or PNG image, fool.'
+    }
 end
--- test/models/issue_test.rb
+++ test/models/issue_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class IssueTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "issue attributes must not be empty" do 
+  	issue = Issue.new
+
+    assert issue.invalid?
+	assert issue.errors[:client].any?
+	assert issue.errors[:name].any?
+	assert issue.errors[:date].any?
+	assert issue.errors[:content].any?
+		
+  end
 end
